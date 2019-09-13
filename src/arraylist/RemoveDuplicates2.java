be0b7bc1b5a623029d4d0576fdd42517e4d5e210
Ljl233 <2955441566@qq.com>
2019-09-13 11:48:12 +0800

    RemoveDuplicates_polish

---
+++ src/arraylist/RemoveDuplicates2.java
@@ -0,0 +1,20 @@
+package arraylist;
+
+public class RemoveDuplicates2 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) return 0;
+        if (nums.length == 1) return 1;
+        int i = 0, j = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+
+        }
+        return i;
+    }
+}
