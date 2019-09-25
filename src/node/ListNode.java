a3d342ce8cebf22e5c4451db44e2a5606648695d
Ljl233 <2955441566@qq.com>
2019-09-25 11:37:10 +0800

    9.25

--- src/node/ListNode.java
+++ src/node/ListNode.java
@@ -7,4 +7,28 @@ public class ListNode {
     ListNode(int x) {
         val = x;
     }
+
+
+    void traverse(ListNode head) {
+        if (head == null) System.out.println("LinkNode is null!");
+
+        while (head != null) {
+            System.out.print(head.val + "      ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    ListNode addP(int[] a) {
+        ListNode res = new ListNode(0);
+        ListNode head = res;
+        res.val = a[0];
+        for (int i = 1; i < a.length; i++) {
+            ListNode next = new ListNode(a[i]);
+            res.next=next;
+            res=next;
+        }
+        traverse(head);
+        return head;
+    }
 }
--- src/node/NodeTest.java
+++ src/node/NodeTest.java
@@ -2,12 +2,19 @@ package node;
 
 public class NodeTest {
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(2);
-        head.next.next.next = new ListNode(1);
-        Boolean bool = new Palindrome().isPalindrome(head);
-        System.out.println(bool);
+//        ListNode head = new ListNode(1);
+//
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(2);
+//        head.next.next.next = new ListNode(1);
+//        Boolean bool = new Palindrome().isPalindrome(head);
+//        System.out.println(bool);
+        int[] a1=new int[]{2,4,3};
+        int[] a2=new int[]{5,6,4};
+        ListNode l1 = new ListNode(0);
+        l1=l1.addP(a1);
+        ListNode l2 = new ListNode(0);
+        l2=l2.addP(a2);
+        new AddTwoNumbers().addTwoNumbers(l1, l2);
     }
 }
