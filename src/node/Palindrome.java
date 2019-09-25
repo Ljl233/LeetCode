0295efcde26960349f6152b16d28dad84f3cd7ef
Ljl233 <2955441566@qq.com>
2019-09-25 10:27:48 +0800

    9.25

--- src/node/Palindrome.java
+++ src/node/Palindrome.java
@@ -1,5 +1,17 @@
 package node;
 
+/**
+ * 请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ *
+ * 输入: 1->2->2->1
+ * 输出: true
+ */
 public class Palindrome {
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
