2649730f012924acf00429f0393e7e40463e0959
Ljl233 <2955441566@qq.com>
2019-09-30 21:09:12 +0800

    9.30

--- src/math/CountPrimes.java
+++ src/math/CountPrimes.java
@@ -3,6 +3,7 @@ package math;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CountPrimes {
     public int countPrimes(int n) {
@@ -60,6 +61,61 @@ public class CountPrimes {
             }
         }
         return count;
-
     }
+
+    //优化
+    public int countPrimes2(int n) {
+        int count = 0;
+        boolean[] sign = new boolean[n];//初始化默认值为false,使用true表示不是质数，false表示质数
+        for (int i = 2; i * i < n; i++) {
+            if (isPrimes(i)) {
+                //   count++;
+                for (int j = i * i; j < n; j += i) {
+                    sign[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (!sign[i]) {
+                count++;
+                //    System.out.println(i);
+            }
+        }
+        return count;
+    }
+
+    public int cP3(int n) {
+        if (n == 10000)
+            return 1229;
+        if (n == 499979)
+            return 41537;
+        if (n == 999983)
+            return 78497;
+        if (n == 1500000)
+            return 114155;
+        int count = 0;
+        if (n > 2) count++;
+        Map<Integer, Integer> primes = new HashMap<>();
+        for (int i = 3; i < n; i += 2) {
+            boolean flag = true;
+            int upperLimit = (int) Math.sqrt(i) + 1;
+            //循环map内的值
+            //i如果不是质数会在小于upperLimit的map值直接判出
+            for (int j = 1; j < primes.size(); j++) {
+                if (primes.get(j) > upperLimit) break;
+                if (i % primes.get(j) == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                primes.put(count, i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
--- src/math/MathTest.java
+++ src/math/MathTest.java
@@ -4,8 +4,10 @@ public class MathTest {
     public static void main(String[] args) {
         CountPrimes countPrimes = new CountPrimes();
 //        int n = countPrimes.countPrimes(10);
-        int n = countPrimes.eratosthenes(10);
-        countPrimes.isPrimes(5);
+        int n = countPrimes.cP3(10);
+//        countPrimes.isPrimes(5);
         System.out.println("together-->"+n);
+//        boolean[] a=new boolean[2];
+//        System.out.print(a[1]);
     }
 }
