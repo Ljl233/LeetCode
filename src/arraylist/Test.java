354e046df81ab260321ac2ffc5cee85a7c3000c6
Ljl233 <2955441566@qq.com>
2019-10-05 22:44:19 +0800

    1

--- src/arraylist/Test.java
+++ src/arraylist/Test.java
@@ -2,11 +2,14 @@ package arraylist;
 
 public class Test {
     public static void main(String[] args) {
-        RemoveDuplicates r = new RemoveDuplicates();
-        int[] a = {1,1,2};
-        r.removeDuplicates(a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
+//        RemoveDuplicates r = new RemoveDuplicates();
+//        int[] a = {1,1,2};
+//        r.removeDuplicates(a);
+//        for (int i = 0; i < a.length; i++) {
+//            System.out.println(a[i]);
+//        }
+        TwoSum twoSum = new TwoSum();
+        twoSum.twoSum(new int[]{2, 3, 4}, 6);
+
     }
 }
---
+++ src/arraylist/TwoSum.java
@@ -0,0 +1,22 @@
+package arraylist;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+       // Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+
+        }
+        return ans;
+
+    }
+}
