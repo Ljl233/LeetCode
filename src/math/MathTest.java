5465bcbf6737450822683e3f703c1efb90921167
Ljl233 <2955441566@qq.com>
2019-10-12 23:46:06 +0800

    10.12　 279'完全平方数

--- src/math/MathTest.java
+++ src/math/MathTest.java
@@ -3,16 +3,21 @@ package math;
 public class MathTest {
     public static void main(String[] args) {
 //        CountPrimes countPrimes = new CountPrimes();
-////        int n = countPrimes.countPrimes(10);
+//       int n = countPrimes.countPrimes(10);
 //        int n = countPrimes.cP3(10);
-////        countPrimes.isPrimes(5);
+//        countPrimes.isPrimes(5);
 //        System.out.println("together-->"+n);
 ////        boolean[] a=new boolean[2];
 ////        System.out.print(a[1]);
 
 //        PowerOfThree power = new PowerOfThree();
-        RomanToInt roman = new RomanToInt();
-        int ans = roman.romanToInt("MCMXCIV");
-        System.out.println(ans);
+//        RomanToInt roman = new RomanToInt();
+//        int ans = roman.romanToInt("MCMXCIV");
+//        System.out.println(ans);
+
+        //numberSquare test
+        NumberSquare numberSquare = new NumberSquare();
+        int a = numberSquare.numSquare(8);
+        System.out.println(a);
     }
 }
