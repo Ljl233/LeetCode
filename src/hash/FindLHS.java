f1776f4795e589bba7d223e453f4d5686510b1a4
Ljl233 <2955441566@qq.com>
2019-11-09 19:00:41 +0800

    594'

---
+++ src/hash/FindLHS.java
@@ -0,0 +1,31 @@
+package hash;
+/*
+和谐数组是指一个数组里元素的最大值和最小值之间的差别正好是1。
+
+        现在，给定一个整数数组，你需要在所有可能的子序列中找到最长的和谐子序列的长度。
+
+        示例 1:
+
+        输入: [1,3,2,2,5,2,3,7]
+        输出: 5
+        原因: 最长的和谐数组是：[3,2,2,2,3].
+        说明: 输入的数组长度最大不超过20,000.*/
+
+import java.util.HashMap;
+
+public class FindLHS {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int ans = 0;
+        for (int num : nums) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+            if (hashMap.containsKey(num + 1)) {
+                ans = Math.max(ans, hashMap.get(num) + hashMap.get(num + 1));
+            }
+            if (hashMap.containsKey(num - 1)) {
+                ans = Math.max(ans, hashMap.get(num) + hashMap.get(num - 1));
+            }
+        }
+        return ans;
+    }
+}
