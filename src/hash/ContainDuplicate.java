f071eb6d479558910fea8b13d81333334e59591e
Ljl233 <2955441566@qq.com>
2019-11-21 21:29:04 +0800

    11.20 217'

---
+++ src/hash/ContainDuplicate.java
@@ -0,0 +1,17 @@
+package hash;
+
+import java.util.HashSet;
+
+public class ContainDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        //
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else set.add(num);
+        }
+        return false;
+    }
+}
