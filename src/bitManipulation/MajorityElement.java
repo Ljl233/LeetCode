ececaa270b7cf6860f455ba01130b30774e500cf
Ljl233 <2955441566@qq.com>
2020-03-11 22:28:53 +0800

    #169

---
+++ src/bitManipulation/MajorityElement.java
@@ -0,0 +1,17 @@
+package bitManipulation;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) candidate = nums[i];
+            count += (nums[i] == candidate ? 1 : -1);
+        }
+
+
+        return candidate;
+
+    }
+}
