f15d280ab33e2ec59aa5202c2920a01bd63aaa1e
Ljl233 <2955441566@qq.com>
2020-07-24 18:17:08 +0800

    后序遍历

---
+++ LeetcodeKt/src/tree/PostorderTraversal.kt
@@ -0,0 +1,69 @@
+package tree
+
+import java.util.*
+
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class PostorderTraversal {
+//    fun postorderTraversal(root: TreeNode?): List<Int> {
+//        val list = mutableListOf<Int>()
+//        postorderTraversal(root, list)
+//        return list
+//    }
+
+    fun postorderTraversal(root: TreeNode?, list: MutableList<Int>) {
+        if (root != null) {
+            postorderTraversal(root.left, list)
+            postorderTraversal(root.right, list)
+            list.add(root.value)
+        }
+    }
+
+    fun postorder(root: TreeNode?): List<Int> {
+        val list = mutableListOf<Int>()
+        val stack = ArrayDeque<TreeNode>()
+
+        var cur = root
+        while (cur != null || stack.isNotEmpty()) {
+            if (cur?.left != null) {
+                stack.push(cur)
+                if (cur.right != null)
+                    stack.push(cur.right)
+                cur = cur.left
+            } else {
+                if (cur != null) {
+                    if (cur.right != null) {
+                        stack.push(cur)
+                        cur = cur.right
+                    } else {
+                        list.add(cur.value)
+                        cur = stack.pop()
+                    }
+                }
+            }
+        }
+        return list
+    }
+
+    fun postorderTraversal(root: TreeNode?): List<Int> {
+        val stack = LinkedList<TreeNode?>()
+        val output = LinkedList<Int>()
+        stack.add(root ?: return output)
+        while (stack.isNotEmpty()) {
+            val node = stack.poll() ?: break
+            output.push(node.value)
+            node.left?.let(stack::push)
+            node.right?.let(stack::push)
+        }
+        return output
+    }
+}
